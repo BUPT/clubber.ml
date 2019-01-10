0e8c5bdc7f3d3357c3a57d3e21396dcd1edc045f
Huan LI <zixia@zixia.net>
2019-01-10 15:31:08 +0800

    add makefile & fit-image scripts

---
+++ scripts/fit-image.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+DSTDIR=${1:-.}
+
+cd ${DSTDIR}
+echo "${DSTDIR}"
+
+FILES=$(git ls-files --exclude-standard --others *.jpg)
+
+mogrify \
+  -verbose \
+  -resize 1920 \
+  ${FILES}
+
+cd -
