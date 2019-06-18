8d66ae3eff83efbb639f1400dac3e0de547d4728
Huan LI <zixia@zixia.net>
2019-06-18 14:30:07 +0800

    fix pr title bug

---
+++ src/pr-number-to-title.ts
@@ -0,0 +1,16 @@
+import { FileBox } from 'file-box'
+
+export async function prNumberToTitle (org: string, repo: string, pr: number): Promise<string> {
+  const fileBox = FileBox.fromUrl(
+    `https://api.github.com/repos/${org}/${repo}/pulls/${pr}`,
+    'pr.json',
+    {
+      'User-Agent': 'FileBox',
+    }
+  )
+  const prJsonText = (await fileBox.toBuffer()).toString()
+  // console.log(prJsonText)
+  const prJson = JSON.parse(prJsonText)
+  const prTitle = prJson.title as string
+  return prTitle
+}
